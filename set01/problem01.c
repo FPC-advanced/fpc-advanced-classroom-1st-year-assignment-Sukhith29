8ca0e4c92f4d4978cb8b90fc22ec5b2c3d6852f7
sukhith.s poojary <sukhithsp.bsc23@rvu.edu.in>
2023-10-17 15:53:14 +0530

    committed 10/17/2023, 3:53:13 PM

--- set01/problem01.c
+++ set01/problem01.c
@@ -1,5 +1,4 @@
 // 1. Write a C program to print your name.
-<<<<<<< HEAD
 #include <stdio.h>
 
 int main()
@@ -8,5 +7,4 @@ int main()
         return 0;
 
     }
-=======
->>>>>>> cae1c3b01a4b1a4b9c35570ca243ae75415f88a3
+
