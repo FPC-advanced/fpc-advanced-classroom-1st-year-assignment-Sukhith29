283eecf51eba5f8dd25cb7dc827fbda310f9585d
sukhith.s poojary <sukhithsp.bsc23@rvu.edu.in>
2023-10-22 20:57:52 +0530

    committed 10/22/2023, 8:57:51 PM

---
+++ set02/problem04.c
@@ -0,0 +1,78 @@
+/*04.  Write a program to find Sum of composite number in an array of different numbers entered by the user.
+
+***Function Declarations***
+```c
+int input_array_size();
+void input_array(int n, int a[n]);
+int sum_composite_numbers(int n, int a[n]);
+void output(int sum);
+```
+
+***Input***
+```
+1 2 7 8 12
+```
+
+***Output***
+```
+2o*/
+#include <stdio.h>
+
+int input_array_size()
+{
+    int n;
+    printf("enter the size of the array :");
+    scanf("%d",&n);
+    return n;
+}
+
+void input_array(int n, int a[n])
+{
+    int i=0;
+    a[n];
+    for (i=0;i<n;i++)
+    {
+        printf("enter the elements of the array :");
+        scanf("%d",a[i]);
+
+    }
+}
+
+int sum_composite_numbers(int n, int a[n])
+{
+    int i;
+    int a[n];
+    int sum = 0;
+    for(i=2;i<n;i++)
+    {
+        if(a[i] % 2 == 0)
+        {
+            sum = sum + a[i];
+        
+        }
+        
+    }
+    return sum ;
+}
+
+void output(int sum)
+{
+    printf("the sum is : %d",sum);
+}
+
+int main ()
+{
+     int n,sum;
+     n=input_array_size();
+     int a[n];
+     input_array(n,a);
+     sum=sum_composite_numbers(n,a);
+     output(sum);
+     return 0;
+
+}
+
+
+
+
+
