23f5f452d6f067669f84201b7012e02ef9e5c9c5
sukhith.s poojary <sukhithsp.bsc23@rvu.edu.in>
2023-11-02 16:21:09 +0530

    committed 11/2/2023, 4:21:08 PM

--- set03/problem01.c
+++ set03/problem01.c
@@ -49,9 +49,9 @@ void output(float x1, float y1, float x2, float y2, float distance)
 
 int main()
 {
-    float x1,x2,y1,y2,diff,Distance;
+    float x1,x2,y1,y2,distance;
     input(&x1,&y1,&x2,&y2);
-    diff=find_distance();
-    output(x1,y1,x2,y2,diff);
+    distance=find_distance();
+    output(x1,y1,x2,y2,distance);
     return 0;
 }
