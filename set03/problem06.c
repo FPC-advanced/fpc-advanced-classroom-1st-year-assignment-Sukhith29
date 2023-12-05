baf156c5d7f063e7e229f137577ebb4329aea1a2
sukhith.s poojary <sukhithsp.bsc23@rvu.edu.in>
2023-12-05 13:45:05 +0530

    committed 12/5/2023, 1:45:04 PM

--- set03/problem06.c
+++ set03/problem06.c
@@ -1,71 +1,45 @@
-/*6. Write a program to find the index of a substring of a string
-
-***Function Declarations***
-```c
-void input_string(char* a, char* b);
-int sub_str_index(char* string, char* substring);
-void output(char *string, char *substring, int index);
-```
-
-***Input***
-```
-helloworldhello
-world
-```
-
-***Output***
-```
-The index of 'world' in 'helloworldhello' is 5
-```*/
-
 #include <stdio.h>
-#include <string.h>
-
-void input_string(char* a, char* b) {
-    printf("Enter the main string: ");
-    scanf("%s", a);
-    
-    printf("Enter the substring: ");
-    scanf("%s", b);
+void input_string(char *a, char *b);
+int sub_str_index(char *string, char *substring);
+void output(char *string, char *substring, int index);
+int main()
+{
+    char string[100], substring[100];
+    int index;
+    input_string(&string, &substring);
+    index = sub_str_index(&string, &substring);
+    output(string, substring, index);
+    return 0;
 }
-
-int sub_str_index(char* string, char* substring) {
-    int len1 = strlen(string);
-    int len2 = strlen(substring);
-
-    for (int i = 0; i <= len1 - len2; i++) {
-        int j;
-
-        for (j = 0; j < len2; j++) {
-            if (string[i +j] != substring[j]) {
-                break;
-            }
-        }
-
-        if (j == len2) {
-            return i;
+void input_string(char *a, char *b)
+{
+    printf("Enter a string:\n");
+    scanf(" %s", a);
+    printf("Enter a substring to find:\n");
+    scanf(" %s", b);
+}
+int sub_str_index(char *string, char *substring)
+{
+    int i, j;
+    for (i = 0; string[i] != '\0'; i++)
+    {
+        for (j = 0; substring[j] != '\0' && string[i + j] == substring[j]; j++)
+            ;
+        if (substring[j] == string[i + j])
+        {
+            return j;
         }
     }
-
     return -1;
 }
-
-void output(char *string, char *substring, int index) {
-    if (index != -1) {
-        printf("The index of '%s' in '%s' is %d\n", substring, string, index);
-    } else {
-        printf("'%s' not found in '%s'\n", substring, string);
+void output(char *string, char *substring, int index)
+{
+    if (index != -1)
+    {
+        printf("The index of %s in %s is %d\n", substring, string, index);
     }
-}
-
-int main() {
-    char main_string[100], sub_string[50];
-    
-    input_string(main_string, sub_string);
-    
-    int index = sub_str_index(main_string, sub_string);
-    
-    output(main_string, sub_string, index);
-
-    return 0;
+    else
+    {
+        printf(" %s is  not a substring of %s", substring, string);
+    }
 }
