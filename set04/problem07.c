76424390e35002760d1d4a3aca6d9f4e8af5f4f1
sukhith.s poojary <sukhithsp.bsc23@rvu.edu.in>
2023-12-05 13:53:06 +0530

    committed 12/5/2023, 1:53:04 PM

--- set04/problem07.c
+++ set04/problem07.c
@@ -26,54 +26,60 @@ void output(Fraction f1, Fraction f2, Fraction f3, Fraction sum)
 ---*/
 
 #include <stdio.h>
-
-typedef struct {
+typedef struct
+{
     int num, den;
 } Fraction;
 
-Fraction input_fraction() {
-    Fraction f;
-    printf("Enter the numerator and denominator of the fraction: ");
-    scanf("%d %d", &f.num, &f.den);
-    return f;
-}
-
-int find_gcd(int a, int b) {
-    while (b != 0) {
-        int temp = b;
-        b = a % b;
-        a = temp;
-    }
-    return a;
-}
-
-Fraction add_fractions(Fraction f1, Fraction f2) {
-    Fraction sum;
-    sum.num = f1.num * f2.den + f2.num * f1.den;
-    sum.den = f1.den * f2.den;
-
-    int gcd = find_gcd(sum.num, sum.den);
-
-    sum.num /= gcd;
-    sum.den /= gcd;
-
-    return sum;
-}
-
-void output(Fraction f1, Fraction f2, Fraction sum) {
-    printf("%d/%d + %d/%d = %d/%d\n", f1.num, f1.den, f2.num, f2.den, sum.num, sum.den);
-}
-
-int main() {
-    Fraction fraction1, fraction2, sum;
-
-    fraction1 = input_fraction();
-    fraction2 = input_fraction();
-
-    sum = add_fractions(fraction1, fraction2);
-
-    output(fraction1, fraction2, sum);
-
+Fraction input_fraction();
+int find_gcd(int a, int b);
+Fraction add_fractions(Fraction f1, Fraction f2);
+void output(Fraction f1, Fraction f2, Fraction sum);
+int main()
+{
+    Fraction f1, f2, sum;
+    f1 = input_fraction();
+    f2 = input_fraction();
+    sum = add_fractions(f1, f2);
+    output(f1, f2, sum);
     return 0;
 }
 
+Fraction input_fraction()
+{
+    Fraction x;
+    scanf("%d%d", &x.num, &x.den);
+    return x;
+}
+
+int find_gcd(int a, int b)
+{
+    int gcd, k;
+
+    k = a < b ? a : b;
+
+    for (int i = 1; i <= k; i++)
+    {
+        if (a % i == 0 && b % i == 0)
+        {
+            gcd = i;
+        }
+    }
+    return gcd;
+}
+
+Fraction add_fractions(Fraction f1, Fraction f2)
+{
+    Fraction sum;
+    sum.num = (f1.num * f2.den) + (f2.num * f1.den);
+    sum.den = f1.den * f2.den;
+    int gcd = find_gcd(sum.num, sum.den);
+    sum.num /= gcd;
+    sum.den /= gcd;
+    return sum;
+}
+
+void output(Fraction f1, Fraction f2, Fraction sum)
+{
+    printf("%d/%d + %d/%d = %d/%d\n", f1.num, f1.den, f2.num, f2.den, sum.num, sum.den);
+}
