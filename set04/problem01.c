e813f78cc350f69fa8061289eddb1bbeb001bc29
sukhith.s poojary <sukhithsp.bsc23@rvu.edu.in>
2023-12-05 12:59:37 +0530

    committed 12/5/2023, 12:59:36 PM

--- set04/problem01.c
+++ set04/problem01.c
@@ -19,30 +19,34 @@ void output(int num1, int den1, int num2, int den2, int res_num, int res_den);
 ```
 
 ---*/
-#include <stdio.h>
-
-void input(int *num1, int *den1, int *num2, int *den2) {
-    printf("Enter the numerator and denominator of the first number: ");
-    scanf("%d %d", num1, den1);
-    printf("Enter the numerator and denominator of the second number: ");
-    scanf("%d %d", num2, den2);
+#include<stdio.h>
+void input(int *num1, int *den1, int *num2, int *den2){
+    printf("enter the first fraction");
+    scanf("%d%d",num1,den1);
+      printf("enter the second fraction");
+    scanf("%d%d",num2,den2);
 }
-
-void add(int num1, int den1, int num2, int den2, int *res_num, int *res_den) {
-    *res_num = (num1 * den2) + (num2 * den1);
-    *res_den = den1 * den2;
+void add(int num1, int den1, int num2, int den2, int *res_num, int *res_den){
+    if(den1!=den2){
+        num1 *=den2;
+        num2 *=den1;
+        *res_num = num1 + num2;
+       *res_den = den1*den2;
 }
-
-void output(int num1, int den1, int num2, int den2, int res_num, int res_den) {
-    printf("The result of %d/%d + %d/%d is %d/%d\n", num1, den1, num2, den2, res_num, res_den);
+else{
+    *res_num = num1 + num2;
+     *res_den = den1;
 }
-
-int main() {
-    int num1, den1, num2, den2, res_num, res_den;
-
-    input(&num1, &den1, &num2, &den2);
-    add(num1, den1, num2, den2, &res_num, &res_den);
-    output(num1, den1, num2, den2, res_num, res_den);
-
-    return 0;
+ 
+}
+void output(int num1, int den1, int num2, int den2, int res_num, int res_den){
+    printf("%d/%d + %d/%d = %d/%d",num1,den1,num2,den2,res_num,res_den);
+    
+}
+int main(){
+    int n1,d1,n2,d2,resn,resd;
+    input(&n1,&d1,&n2,&d2);
+    add(n1,d1,n2,d2,&resn,&resd);
+    output(n1,d1,n2,d2,resn,resd);
+   return 0;
 }
