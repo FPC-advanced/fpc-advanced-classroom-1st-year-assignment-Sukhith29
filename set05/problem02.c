dc7845e8e2ef1f4a5bff5e944663940ddd2cb20c
sukhith.s poojary <sukhithsp.bsc23@rvu.edu.in>
2023-12-05 13:56:36 +0530

    committed 12/5/2023, 1:56:34 PM

--- set05/problem02.c
+++ set05/problem02.c
@@ -26,30 +26,30 @@ The weight of the camel with radius: 1.0, height: 1.0, length: 1.0 is 3.1415
 
 #include <stdio.h>
 #include <math.h>
-
-#define PI 3.1415
-
-void input_camel_details(float *radius, float *height, float *length) {
-    printf("Enter the stomach radius, height, and length of the camel: ");
+void input_camel_details(float *radius, float *height, float *length);
+float find_weight(float radius, float height, float length);
+void output(float radius, float height, float length, float weight);
+int main()
+{
+    float radius, height, length, weight;
+    input_camel_details(&radius, &height, &length);
+    weight = find_weight(radius, height, length);
+    output(radius, height, length, weight);
+    return 0;
+}
+void input_camel_details(float *radius, float *height, float *length)
+{
+    printf("Enter the radius,height and length of the Camel:\n");
     scanf("%f %f %f", radius, height, length);
 }
-
-float find_weight(float radius, float height, float length) {
-    return PI * pow(radius, 3) * sqrt(height * length);
+float find_weight(float radius, float height, float length)
+{
+    float weight, pi;
+    pi = 3.142;
+    weight = pi * radius * radius * radius * sqrt(height * length);
+    return weight;
 }
-
-void output(float radius, float height, float length, float weight) {
-    printf("The weight of the camel with radius: %.1f, height: %.1f, length: %.1f is %.4f\n", radius, height, length, weight);
-}
-
-int main() {
-    float radius, height, length, weight;
-
-    input_camel_details(&radius, &height, &length);
-
-    weight = find_weight(radius, height, length);
-
-    output(radius, height, length, weight);
-
-    return 0;
+void output(float radius, float height, float length, float weight)
+{
+    printf("The weight of the camel with radius: %0.1f, height: %0.1f, length: %0.1f is %f", radius, height, length, weight);
 }
