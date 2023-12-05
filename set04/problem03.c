24860135801b9a6957b8d736de9594a5a8242924
sukhith.s poojary <sukhithsp.bsc23@rvu.edu.in>
2023-12-05 13:47:35 +0530

    committed 12/5/2023, 1:47:34 PM

--- set04/problem03.c
+++ set04/problem03.c
@@ -1,60 +1,31 @@
-/*3. Write a program to find the `nCr` of given n and r
-
-***Function Declarations***
-```c
+#include <stdio.h>
 void input_n_and_r(int *n, int *r);
 int nCr(int n, int r);
 void output(int n, int r, int result);
-```
-
-***Input***
-```
-6
-3
-```
-
-***Output***
-```
-for n = 6 and r = 3, nCr = 20
-```
-
----*/
-
-#include <stdio.h>
-
-void input_n_and_r(int *n, int *r) {
-    printf("Enter the values for n and r: \n");
-    scanf("%d %d", n, r);
+int main()
+{
+   int n, r, result;
+   input_n_and_r(&n, &r);
+   result = nCr(n, r);
+   output(n, r, result);
+   return 0;
 }
-
-unsigned long long factorial(int num) {
-    if (num == 0 || num == 1) {
-        return 1;
-    } else {
-        unsigned long long result = 1;
-        for (int i = 2; i <= num; ++i) {
-            result *= i;
-        }
-        return result;
-    }
+void input_n_and_r(int *n, int *r)
+{
+   printf("Enter the value of n ,r:\n");
+   scanf("%d%d", n, r);
 }
-
-unsigned long long nCr(int n, int r) {
-    return factorial(n) / (factorial(r) * factorial(n - r));
+int nCr(int n, int r)
+{
+   int i, ncr = 1;
+   for (i = 1; i <= r; i++)
+   {
+      ncr *= (n - i + 1);
+      ncr /= i;
+   }
+   return ncr;
 }
-
-void output(int n, int r, unsigned long long result) {
-    printf("For n = %d and r = %d, nCr = %llu\n", n, r, result);
-}
-
-int main() {
-    int n, r;
-
-    input_n_and_r(&n, &r);
-
-    unsigned long long result = nCr(n, r);
-
-    output(n, r, result);
-
-    return 0;
+void output(int n, int r, int result)
+{
+   printf("for n=%d and r=%d,nCr = %d\n", n, r, result);
 }
